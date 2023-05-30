a466eb5d4ff0a6c0d5b91453ac8e0341a8513308
Prashant Pacific <30399370+Pacific-Pacifico@users.noreply.github.com>
2023-05-30 14:46:06 +0530

    refactoring

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,9 @@ import Home from "./pages/Home";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
-import Wishlist from "./pages/wishlist";
-import Cart from "./pages/cart";
-import Orders from "./pages/orders";
 import Settings from "./pages/Settings";
 import Notifications from "./pages/Notifications";
 import Profile from "./pages/Profile";
-import Categories from "./pages/Categories";
-import { SubCategories } from "./pages/Categories";
-import Checkout from "./pages/Checkout";
 
 export const appName = import.meta.env.VITE_APP_NAME;
 
@@ -22,18 +16,9 @@ const App = () => {
           <Route exact path="/" element={<Home />} />
           <Route exact path="/login" element={<Login />} />
           <Route exact path="/register" element={<Register />} />
-          <Route exact path="/wishlist" element={<Wishlist />} />
-          <Route exact path="/cart" element={<Cart />} />
-          <Route exact path="/orders" element={<Orders />} />
           <Route exact path="/profile" element={<Profile />} />
           <Route exact path="/settings" element={<Settings />} />
           <Route exact path="/notifications" element={<Notifications />} />
-          <Route exact path="/recommended-products" element={<Orders />} />
-          <Route exact path="/popular-products" element={<Orders />} />
-          <Route exact path="/new-products" element={<Orders />} />
-          <Route exact path="/categories" element={<Categories />} />
-          <Route exact path="/subcategories" element={<SubCategories />} />
-          <Route exact path="/checkout" element={<Checkout />} />
         </Routes>
       </BrowserRouter>
     </div>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,31 +1,7 @@
-import BannerCarousel from "../components/Carousel";
-import Footer from "../components/Footer";
-import NavBar from "../components/NavBar";
-import Section from "../components/Section";
+import React from "react";
 
 function Home() {
-  return (
-    <>
-      <NavBar />
-      <BannerCarousel />
-      <Section
-        sectionTitle="Recommended For You"
-        linkPath="/recommended-products"
-        endpoint="/recommended-products?limit=6"
-      />
-      <Section
-        sectionTitle="Popular Products"
-        linkPath="/popular-products"
-        endpoint="/popular-products?limit=6"
-      />
-      <Section
-        sectionTitle="New Products"
-        linkPath="/new-products"
-        endpoint="/new-products?limit=6"
-      />
-      <Footer />
-    </>
-  );
+  return <div>Home</div>;
 }
 
 export default Home;
