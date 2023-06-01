beea44fad68d44d0919172d13b036b116293d7ed
Prashant Pacific <30399370+Pacific-Pacifico@users.noreply.github.com>
2023-06-01 16:11:40 +0530

    moved from sidebar to navbar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -12,6 +12,8 @@ import {
   faMagnifyingGlass,
   faBell,
   faHouse,
+  faCubes,
+  faClipboardList,
   faListCheck,
   faRightFromBracket,
   faHeadset,
@@ -34,24 +36,15 @@ function NavBar({ isLoggedIn = true }) {
     }));
   };
   return (
-    <nav className="navbar navbar-expand-lg bg-body-tertiary">
-      <div className="container-fluid">
-        <img src={Logo} alt="KalaKriti" style={{ width: 50, height: 50 }} />
-        <b>{appName + " Seller Studio"}</b>
-        <button
-          className="navbar-toggler"
-          type="button"
-          data-bs-toggle="collapse"
-          data-bs-target="#navbarSupportedContent"
-          aria-controls="navbarSupportedContent"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span className="navbar-toggler-icon" />
-        </button>
-        <div className="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-            <li className="nav-item me-2">
+    <div className="px-3 py-2 text-bg-dark border-bottom">
+      <div className="container">
+        <div className="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
+          <div className="d-flex align-items-center my-2 my-lg-0 me-lg-auto text-white text-decoration-none">
+            <img src={Logo} alt="KalaKriti" style={{ width: 50, height: 50 }} />
+            <b>{appName + " Seller Studio"}</b>
+          </div>
+          <ul className="nav col-12 col-lg-auto my-2 justify-content-center my-md-0 text-small">
+            <li className="nav-item me-3">
               <Link
                 className="nav-link active"
                 aria-current="page"
@@ -65,165 +58,59 @@ function NavBar({ isLoggedIn = true }) {
                   style={{ color: "#dd13a7" }}
                 />
               </Link>
+              Dashboard
             </li>
 
-            {/* <form className="d-flex" role="search">
-              <input
-                className="form-control me-2"
-                type="search"
-                placeholder="Search"
-                aria-label="Search"
-              />
-              <button className="btn border px-2" type="submit" title="search">
+            <li className="nav-item me-3">
+              <Link
+                className="nav-link"
+                aria-current="page"
+                to="/products"
+                title="Products"
+              >
                 <FontAwesomeIcon
-                  icon={faMagnifyingGlass}
+                  icon={faCubes}
                   size="xl"
-                  style={{ color: "#5485c4" }}
-                  onMouseEnter={() => handleIconHovered("search")}
-                  onMouseLeave={() => handleIconHovered("search")}
-                  fade={iconsHoveredState.search ? "fade" : undefined}
+                  style={{ color: "#45b58a" }}
                 />
-              </button>
-            </form> */}
+              </Link>
+              Products
+            </li>
+            <li className="nav-item me-3">
+              <Link
+                className="nav-link"
+                aria-current="page"
+                to="/orders"
+                title="Orders"
+              >
+                <FontAwesomeIcon
+                  icon={faClipboardList}
+                  size="xl"
+                  style={{ color: "#27511f" }}
+                />
+              </Link>
+              Orders
+            </li>
+            <li>
+              <a href="#" className="nav-link text-white">
+                <svg className="bi d-block mx-auto mb-1" width={24} height={24}>
+                  <use xlinkHref="#grid" />
+                </svg>
+                Products
+              </a>
+            </li>
+            <li>
+              <a href="#" className="nav-link text-white">
+                <svg className="bi d-block mx-auto mb-1" width={24} height={24}>
+                  <use xlinkHref="#people-circle" />
+                </svg>
+                Customers
+              </a>
+            </li>
           </ul>
-          <div
-            className="collapse navbar-collapse justify-content-end"
-            id="navbarSupportedContent"
-          >
-            <ul className="navbar-nav">
-              <li className="nav-item">
-                <Link to="/notifications" title="notifications">
-                  <button type="button" className="btn border px-2 me-2">
-                    {/* <img src={WishlistSvg} alt="Wishlist" /> */}
-                    <FontAwesomeIcon
-                      icon={faBell}
-                      size="xl"
-                      style={{ color: "#f0e03c" }}
-                      onMouseEnter={() => handleIconHovered("notifications")}
-                      onMouseLeave={() => handleIconHovered("notifications")}
-                      shake={
-                        iconsHoveredState.notifications ? "shake" : undefined
-                      }
-                    />
-                  </button>
-                </Link>
-              </li>
-
-              <li className="nav-item">
-                <Link to="/orders" title="orders">
-                  <button type="button" className="btn border px-2 me-2">
-                    {/* My Orders */}
-                    <FontAwesomeIcon
-                      icon={faListCheck}
-                      size="xl"
-                      style={{ color: "#0feb1e" }}
-                      onMouseEnter={() => handleIconHovered("orders")}
-                      onMouseLeave={() => handleIconHovered("orders")}
-                      fade={iconsHoveredState.orders ? "fade" : undefined}
-                    />
-                  </button>
-                </Link>
-              </li>
-              <li className="nav-item">
-                <Link to="/settings" title="settings">
-                  <button type="button" className="btn border px-2 me-2">
-                    {/* <img src={ProfileSvg} alt="Profile" />
-                    Profile */}
-                    <FontAwesomeIcon
-                      icon={faGear}
-                      size="xl"
-                      style={{ color: "#646464" }}
-                      onMouseEnter={() => handleIconHovered("settings")}
-                      onMouseLeave={() => handleIconHovered("settings")}
-                      spin={iconsHoveredState.settings ? "spin" : undefined}
-                    />
-                  </button>
-                </Link>
-              </li>
-              {isLoggedIn ? (
-                <li className="nav-item dropdown">
-                  <Link
-                    className="nav-link dropdown-toggle"
-                    to="/profile"
-                    role="button"
-                    data-bs-toggle="dropdown"
-                    aria-expanded="false"
-                    title="profile"
-                  >
-                    {/* <img src={ProfileSvg} alt="Profile" />
-                    Profile */}
-                    <FontAwesomeIcon
-                      icon={faUser}
-                      size="xl"
-                      style={{ color: "#964B00" }}
-                      onMouseEnter={() => handleIconHovered("profile")}
-                      onMouseLeave={() => handleIconHovered("profile")}
-                      bounce={iconsHoveredState.profile ? "bounce" : undefined}
-                    />
-                    Hello, <b>seller_name</b>
-                  </Link>
-                  <ul className="dropdown-menu dropdown-menu-end">
-                    <li>
-                      <Link className="dropdown-item" to="/viewed-history">
-                        <FontAwesomeIcon
-                          icon={faClockRotateLeft}
-                          style={{ color: "#a45f41" }}
-                        />
-                        &nbsp; Viewed Products
-                      </Link>
-                    </li>
-                    <li>
-                      <Link className="dropdown-item" to="/">
-                        <FontAwesomeIcon
-                          icon={faHeadset}
-                          style={{ color: "#d84f22" }}
-                        />
-                        &nbsp; Support/Help
-                      </Link>
-                    </li>
-                    <li>
-                      <Link className="dropdown-item" to="/">
-                        Become a Seller / Login as Seller
-                      </Link>
-                    </li>
-                    <li>
-                      <hr className="dropdown-divider" />
-                    </li>
-                    <li>
-                      <Link className="dropdown-item" to="/">
-                        <FontAwesomeIcon
-                          icon={faRightFromBracket}
-                          style={{ color: "#4dbad5" }}
-                        />
-                        &nbsp; Logout
-                      </Link>
-                    </li>
-                  </ul>
-                </li>
-              ) : (
-                <>
-                  <li className="nav-item">
-                    <Link to="/login" title="login">
-                      <button type="button" className="btn btn-primary me-2">
-                        Login
-                      </button>
-                    </Link>
-                  </li>
-
-                  <li className="nav-item">
-                    <Link to="/register" title="login">
-                      <button type="button" className="btn btn-success me-2">
-                        Register
-                      </button>
-                    </Link>
-                  </li>
-                </>
-              )}
-            </ul>
-          </div>
         </div>
       </div>
-    </nav>
+    </div>
   );
 }
 
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,8 +5,8 @@ import NavBar from "../components/NavBar";
 function Home() {
   return (
     <>
-      <SideBar />
-      {/* <NavBar /> */}
+      {/* <SideBar /> */}
+      <NavBar />
     </>
   );
 }
