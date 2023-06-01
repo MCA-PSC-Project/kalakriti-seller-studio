c968895453f60dacfa3850c9af1151e68dbdb67f
Prashant Pacific <30399370+Pacific-Pacifico@users.noreply.github.com>
2023-06-01 12:39:20 +0530

    sidebar vh fixed

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -16,7 +16,7 @@ import {
 function SideBar() {
   return (
     <>
-      <header className="navbar navbar-light sticky-top bg-light flex-md-nowrap p-0 shadow">
+      <header className="navbar sticky-top bg-light flex-md-nowrap p-0 shadow">
         <div className="">
           <img src={Logo} alt="KalaKriti" style={{ width: 50, height: 50 }} />
           <b>{appName + " Seller Studio"}</b>
@@ -44,7 +44,7 @@ function SideBar() {
         <div className="row">
           <nav
             id="sidebarMenu"
-            className="col-md-3 col-lg-2 d-md-block bg-light sidebar collapse"
+            className="col-md-3 col-lg-2 d-md-block sidebar collapse vh-100 bg-light"
             style={{}}
           >
             <div className="position-sticky pt-3 sidebar-sticky">
@@ -154,194 +154,7 @@ function SideBar() {
               </div>
             </div>
           </nav>
-          {/* <main className="col-md-9 ms-sm-auto col-lg-10 px-md-4">
-            <div className="chartjs-size-monitor">
-              <div className="chartjs-size-monitor-expand">
-                <div className="" />
-              </div>
-              <div className="chartjs-size-monitor-shrink">
-                <div className="" />
-              </div>
-            </div>
-            <div className="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
-              <h1 className="h2">Dashboard</h1>
-              <div className="btn-toolbar mb-2 mb-md-0">
-                <div className="btn-group me-2">
-                  <button
-                    type="button"
-                    className="btn btn-sm btn-outline-secondary"
-                  >
-                    Share
-                  </button>
-                  <button
-                    type="button"
-                    className="btn btn-sm btn-outline-secondary"
-                  >
-                    Export
-                  </button>
-                </div>
-                <button
-                  type="button"
-                  className="btn btn-sm btn-outline-secondary dropdown-toggle"
-                >
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    width={24}
-                    height={24}
-                    viewBox="0 0 24 24"
-                    fill="none"
-                    stroke="currentColor"
-                    strokeWidth={2}
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    className="feather feather-calendar align-text-bottom"
-                    aria-hidden="true"
-                  >
-                    <rect x={3} y={4} width={18} height={18} rx={2} ry={2} />
-                    <line x1={16} y1={2} x2={16} y2={6} />
-                    <line x1={8} y1={2} x2={8} y2={6} />
-                    <line x1={3} y1={10} x2={21} y2={10} />
-                  </svg>
-                  This week
-                </button>
-              </div>
-            </div>
-            <canvas
-              className="my-4 w-100 chartjs-render-monitor"
-              id="myChart"
-              width={1098}
-              height={462}
-              style={{ display: "block", height: 154, width: 366 }}
-            />
-            <h2>Section title</h2>
-            <div className="table-responsive">
-              <table className="table table-striped table-sm">
-                <thead>
-                  <tr>
-                    <th scope="col">#</th>
-                    <th scope="col">Header</th>
-                    <th scope="col">Header</th>
-                    <th scope="col">Header</th>
-                    <th scope="col">Header</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  <tr>
-                    <td>1,001</td>
-                    <td>random</td>
-                    <td>data</td>
-                    <td>placeholder</td>
-                    <td>text</td>
-                  </tr>
-                  <tr>
-                    <td>1,002</td>
-                    <td>placeholder</td>
-                    <td>irrelevant</td>
-                    <td>visual</td>
-                    <td>layout</td>
-                  </tr>
-                  <tr>
-                    <td>1,003</td>
-                    <td>data</td>
-                    <td>rich</td>
-                    <td>dashboard</td>
-                    <td>tabular</td>
-                  </tr>
-                  <tr>
-                    <td>1,003</td>
-                    <td>information</td>
-                    <td>placeholder</td>
-                    <td>illustrative</td>
-                    <td>data</td>
-                  </tr>
-                  <tr>
-                    <td>1,004</td>
-                    <td>text</td>
-                    <td>random</td>
-                    <td>layout</td>
-                    <td>dashboard</td>
-                  </tr>
-                  <tr>
-                    <td>1,005</td>
-                    <td>dashboard</td>
-                    <td>irrelevant</td>
-                    <td>text</td>
-                    <td>placeholder</td>
-                  </tr>
-                  <tr>
-                    <td>1,006</td>
-                    <td>dashboard</td>
-                    <td>illustrative</td>
-                    <td>rich</td>
-                    <td>data</td>
-                  </tr>
-                  <tr>
-                    <td>1,007</td>
-                    <td>placeholder</td>
-                    <td>tabular</td>
-                    <td>information</td>
-                    <td>irrelevant</td>
-                  </tr>
-                  <tr>
-                    <td>1,008</td>
-                    <td>random</td>
-                    <td>data</td>
-                    <td>placeholder</td>
-                    <td>text</td>
-                  </tr>
-                  <tr>
-                    <td>1,009</td>
-                    <td>placeholder</td>
-                    <td>irrelevant</td>
-                    <td>visual</td>
-                    <td>layout</td>
-                  </tr>
-                  <tr>
-                    <td>1,010</td>
-                    <td>data</td>
-                    <td>rich</td>
-                    <td>dashboard</td>
-                    <td>tabular</td>
-                  </tr>
-                  <tr>
-                    <td>1,011</td>
-                    <td>information</td>
-                    <td>placeholder</td>
-                    <td>illustrative</td>
-                    <td>data</td>
-                  </tr>
-                  <tr>
-                    <td>1,012</td>
-                    <td>text</td>
-                    <td>placeholder</td>
-                    <td>layout</td>
-                    <td>dashboard</td>
-                  </tr>
-                  <tr>
-                    <td>1,013</td>
-                    <td>dashboard</td>
-                    <td>irrelevant</td>
-                    <td>text</td>
-                    <td>visual</td>
-                  </tr>
-                  <tr>
-                    <td>1,014</td>
-                    <td>dashboard</td>
-                    <td>illustrative</td>
-                    <td>rich</td>
-                    <td>data</td>
-                  </tr>
-                  <tr>
-                    <td>1,015</td>
-                    <td>random</td>
-                    <td>tabular</td>
-                    <td>information</td>
-                    <td>text</td>
-                  </tr>
-                </tbody>
-              </table>
-            </div>
-          </main> */}
+          {/* <main className="col-md-9 ms-sm-auto col-lg-10 px-md-4"></main> */}
         </div>
       </div>
     </>
