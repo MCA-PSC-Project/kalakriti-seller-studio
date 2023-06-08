6029ccaded9320a230ed78fabc626b9487b55a64
sheetal <sheetalkumarilife@gmail.com>
2023-06-08 18:42:29 +0530

    landing page

--- src/components/BannerCarousel.jsx
+++ src/components/BannerCarousel.jsx
@@ -1,140 +1,99 @@
-import { useEffect, useState } from "react";
-import { axiosClient } from "../utils/axios-client";
-import { Link } from "react-router-dom";
+import banner1 from "../assets/banner1.jpg";
+ import banner2 from "../assets/banner2.jpg";
+ import banner3 from "../assets/banner3.jpg";
 
-function BannerCarousel() {
-  return (
-    <div
-      id="myCarousel"
-      className="carousel slide mb-6"
-      data-bs-ride="carousel"
-    >
-      <div className="carousel-indicators">
-        <button
-          type="button"
-          data-bs-target="#myCarousel"
-          data-bs-slide-to={0}
-          className=""
-          aria-label="Slide 1"
-        />
-        <button
-          type="button"
-          data-bs-target="#myCarousel"
-          data-bs-slide-to={1}
-          aria-label="Slide 2"
-          className="active"
-          aria-current="true"
-        />
-        <button
-          type="button"
-          data-bs-target="#myCarousel"
-          data-bs-slide-to={2}
-          aria-label="Slide 3"
-        />
-      </div>
-      <div className="carousel-inner">
-        <div className="carousel-item">
-          <svg
-            className="bd-placeholder-img"
-            width="100%"
-            height="100%"
-            xmlns="http://www.w3.org/2000/svg"
-            aria-hidden="true"
-            preserveAspectRatio="xMidYMid slice"
-            focusable="false"
-          >
-            <rect width="100%" height="100%" fill="var(--bs-secondary-color)" />
-          </svg>
-          <div className="container">
-            <div className="carousel-caption text-start">
-              <h1>Example headline.</h1>
-              <p className="opacity-75">
-                Some representative placeholder content for the first slide of
-                the carousel.
-              </p>
-              <p>
-                <a className="btn btn-lg btn-primary" href="#">
-                  Sign up today
-                </a>
-              </p>
-            </div>
-          </div>
-        </div>
-        <div className="carousel-item active">
-          <svg
-            className="bd-placeholder-img"
-            width="100%"
-            height="100%"
-            xmlns="http://www.w3.org/2000/svg"
-            aria-hidden="true"
-            preserveAspectRatio="xMidYMid slice"
-            focusable="false"
-          >
-            <rect width="100%" height="100%" fill="var(--bs-secondary-color)" />
-          </svg>
-          <div className="container">
-            <div className="carousel-caption">
-              <h1>Another example headline.</h1>
-              <p>
-                Some representative placeholder content for the second slide of
-                the carousel.
-              </p>
-              <p>
-                <a className="btn btn-lg btn-primary" href="#">
-                  Learn more
-                </a>
-              </p>
-            </div>
-          </div>
-        </div>
-        <div className="carousel-item">
-          <svg
-            className="bd-placeholder-img"
-            width="100%"
-            height="100%"
-            xmlns="http://www.w3.org/2000/svg"
-            aria-hidden="true"
-            preserveAspectRatio="xMidYMid slice"
-            focusable="false"
-          >
-            <rect width="100%" height="100%" fill="var(--bs-secondary-color)" />
-          </svg>
-          <div className="container">
-            <div className="carousel-caption text-end">
-              <h1>One more for good measure.</h1>
-              <p>
-                Some representative placeholder content for the third slide of
-                this carousel.
-              </p>
-              <p>
-                <a className="btn btn-lg btn-primary" href="#">
-                  Browse gallery
-                </a>
-              </p>
-            </div>
-          </div>
-        </div>
-      </div>
+function BannerCarousel(){
+  return(
+    <div id="myCarouselExampleIndicators" className="carousel slide" >
+    <div className="carousel-indicators">
       <button
-        className="carousel-control-prev"
         type="button"
-        data-bs-target="#myCarousel"
-        data-bs-slide="prev"
-      >
-        <span className="carousel-control-prev-icon" aria-hidden="true" />
-        <span className="visually-hidden">Previous</span>
-      </button>
+        data-bs-target="#myCarouselExampleIndicators"
+        data-bs-slide-to="0"
+        className="active"
+        aria-label="Slide 1"
+        aria-current="true"
+      ></button>
       <button
-        className="carousel-control-next"
         type="button"
-        data-bs-target="#myCarousel"
-        data-bs-slide="next"
-      >
-        <span className="carousel-control-next-icon" aria-hidden="true" />
-        <span className="visually-hidden">Next</span>
-      </button>
+        data-bs-target="#myCarouselExampleIndicators"
+        data-bs-slide-to="1"
+        aria-label="Slide 2"
+        className=""
+      ></button>
+      <button
+        type="button"
+        data-bs-target="#myCarouselExampleIndicators"
+        data-bs-slide-to="2"
+        aria-label="Slide 3"
+        className=""
+      ></button>
     </div>
-  );
+    <div className="carousel-inner">
+      <div className="carousel-item active">
+        <img
+          src={banner1}
+          className="d-block w-100"
+          alt="..."
+          style={{ width: "100%", height: "500px" }}
+        />
+         <div class="carousel-caption d-none d-md-block">
+         <button type="button" style={{backgroundColor:"black"}} class="btn btn-primary">Sign up Today</button>
+        <h5  style={{fontSize:"40px" , WebkitTextStroke:"1px white", WebkitTextFillColor:"black" }}>First slide label</h5>
+        <p style={{fontSize:"40px" , WebkitTextStroke:"1px white", WebkitTextFillColor:"black" }}><b>
+          Nulla vitae elit libero, a pharetra augue mollis interdum.</b>
+        </p>
+      </div>
+      </div>
+      <div className="carousel-item active">
+        <img
+            src={banner2}
+          className="d-block w-100"
+          alt="..."
+          style={{ width: "100%", height: "500px" }}
+        />
+         <div class="carousel-caption d-none d-md-block">
+        <h5>second slide label</h5>
+        <p>
+          Nulla vitae elit libero, a pharetra augue mollis interdum.
+        </p>
+      </div>
+      </div>
+      <div className="carousel-item">
+        <img
+           src={banner3}
+          className="d-block w-100"
+          alt="..."
+          style={{ width: "100%", height: "500px" }}
+        />
+         <div class="carousel-caption d-none d-md-block">
+        <h5>third slide label</h5>
+        <p>
+          Nulla vitae elit libero, a pharetra augue mollis interdum.
+        </p>
+      </div>
+      </div>
+    </div>
+    <button
+      className="carousel-control-prev"
+      type="button"
+      data-bs-target="#myCarouselExampleIndicators"
+      data-bs-slide="prev"
+    >
+      <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+      <span className="visually-hidden">Previous</span>
+    </button>
+    <button
+      className="carousel-control-next"
+      type="button"
+      data-bs-target="#myCarouselExampleIndicators"
+      data-bs-slide="next"
+    >
+      <span className="carousel-control-next-icon" aria-hidden="true"></span>
+      <span className="visually-hidden">Next</span>
+    </button>
+  </div>
+);
 }
 
 export default BannerCarousel;
--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -4,6 +4,20 @@ import Footer from "../components/Footer";
 import Logo from "../assets/logo.jpeg";
 import { appName } from "../App";
 import { Link } from "react-router-dom";
+import banner1 from "../assets/banner1.jpg";
+
+import feature1 from "../assets/feature1.jpg";
+import feature2 from "../assets/feature2.png";
+import feature3 from "../assets/feature3.jpg";
+import feature4 from "../assets/feature4.jpg";
+import feature5 from "../assets/feature5.jpg";
+
+import sellerCraft1 from "../assets/sellerCraft1.jpg";
+import sellerCraft2 from "../assets/sellerCraft2.jpg";
+import sellerCraft3 from "../assets/sellerCraft3.jpg";
+
+
+
 
 function LandingPage() {
   return (
@@ -12,7 +26,7 @@ function LandingPage() {
       <main>
         <BannerCarousel />
         {/* Wrap the rest of the page in another container to center all the content. */}
-        <div className="container marketing">
+        <div className="container marketing" >
           {/* Three columns of text below the carousel */}
           <div className="row">
             <div className="col-lg-4">
@@ -26,17 +40,21 @@ function LandingPage() {
                 preserveAspectRatio="xMidYMid slice"
                 focusable="false"
               >
+                <image href={feature1} height="150" width="150" />
+
                 <title>Placeholder</title>
-                <rect
+                {/* <rect
                   width="100%"
                   height="100%"
-                  fill="var(--bs-secondary-color)"
-                />
+                 fill="var(--bs-secondary-bg"
+                   
+                /> */}
               </svg>
-              <h2 className="fw-normal">Heading</h2>
+              <h2 className="fw-normal">Growth</h2>
               <p>
-                Some representative placeholder content for the three columns of
-                text below the carousel. This is the first column.
+                Witnessing tremendous growth by selling product online, retailers are
+                moving towards online selling. Avoid huge investments. Large
+                customer base to sell online anywhere.
               </p>
               <p>
                 <a className="btn btn-secondary" href="#">
@@ -56,24 +74,25 @@ function LandingPage() {
                 preserveAspectRatio="xMidYMid slice"
                 focusable="false"
               >
+                <image href={feature2} height="150" width="150" />
                 <title>Placeholder</title>
-                <rect
+                {/* <rect
                   width="100%"
                   height="100%"
                   fill="var(--bs-secondary-color)"
-                />
+                /> */}
               </svg>
-              <h2 className="fw-normal">Heading</h2>
+              <h2 className="fw-normal">Easy Product Listing</h2>
               <p>
-                Another exciting bit of representative placeholder content. This
-                time, we've moved on to the second column.
-              </p>
+              Image guidelines are relaxed and easy to follow. 
+              Sellers can upload tabletop and hanger images clicked with mobile phones.              </p>
               <p>
                 <a className="btn btn-secondary" href="#">
                   View details »
                 </a>
               </p>
             </div>
+    
             {/* /.col-lg-4 */}
             <div className="col-lg-4">
               <svg
@@ -86,18 +105,81 @@ function LandingPage() {
                 preserveAspectRatio="xMidYMid slice"
                 focusable="false"
               >
+                <image href={feature3} height="180" width="150" />
                 <title>Placeholder</title>
-                <rect
+                {/* <rect
                   width="100%"
                   height="100%"
                   fill="var(--bs-secondary-color)"
-                />
+                /> */}
               </svg>
-              <h2 className="fw-normal">Heading</h2>
+              <h2 className="fw-normal">Enhanced reach to millions of customers</h2>
               <p>
-                And lastly this, the third column of representative placeholder
-                content.
+              You capture a huge amount of budget-friendly consumers through resellers.
+               These resellers reach the consumer base through their social media channels.
               </p>
+              <p>
+                <a className="btn btn-secondary" href="#">
+                  View details »
+                </a>
+              </p>
+            </div>
+
+            <div className="col-lg-4">
+              <svg
+                className="bd-placeholder-img rounded-circle"
+                width={140}
+                height={140}
+                xmlns="http://www.w3.org/2000/svg"
+                role="img"
+                aria-label="Placeholder"
+                preserveAspectRatio="xMidYMid slice"
+                focusable="false"
+              >
+                <image href={feature4} height="150" width="150" />
+                <title>Placeholder</title>
+                {/* <rect
+                  width="100%"
+                  height="100%"
+                  fill="var(--bs-secondary-color)"
+                /> */}
+              </svg>
+              <h2 className="fw-normal">The lowest cost of doing business in the industry</h2>
+              <p>
+              With the most competitive rate card in the industry,
+               transparent delivery charges based on the weight and dimensions of your products and a small fixed fee,
+                selling on Flipkart is highly cost-efficient.              </p>
+              <p>
+                <a className="btn btn-secondary" href="#">
+                  View details »
+                </a>
+              </p>
+            </div>
+
+            <div className="col-lg-4">
+              <svg
+                className="bd-placeholder-img rounded-circle"
+                width={140}
+                height={140}
+                xmlns="http://www.w3.org/2000/svg"
+                role="img"
+                aria-label="Placeholder"
+                preserveAspectRatio="xMidYMid slice"
+                focusable="false"
+              >
+                <image href={feature5} height="120" width="100" />
+                <title>Placeholder</title>
+                {/* <rect
+                  width="100%"
+                  height="100%"
+                  fill="var(--bs-secondary-color)"
+                /> */}
+              </svg>
+              <h2 className="fw-normal">0% Commission platform</h2>
+              <p>
+               This season start your business on KalaKriti with 0% commission.
+               
+This will be applicable to verticals listed only on Kalakriti.            </p>
               <p>
                 <a className="btn btn-secondary" href="#">
                   View details »
@@ -131,12 +213,13 @@ function LandingPage() {
                 preserveAspectRatio="xMidYMid slice"
                 focusable="false"
               >
+                <image href={sellerCraft1} height="500" width="800" />
                 <title>Placeholder</title>
-                <rect
+                {/* <rect
                   width="100%"
                   height="100%"
                   fill="var(--bs-secondary-bg)"
-                />
+                /> */}
                 <text
                   x="50%"
                   y="50%"
@@ -172,12 +255,13 @@ function LandingPage() {
                 preserveAspectRatio="xMidYMid slice"
                 focusable="false"
               >
+                <image href={sellerCraft2} height="500" width="800" />
                 <title>Placeholder</title>
-                <rect
+                {/* <rect
                   width="100%"
                   height="100%"
                   fill="var(--bs-secondary-bg)"
-                />
+                /> */}
                 <text
                   x="50%"
                   y="50%"
@@ -214,12 +298,13 @@ function LandingPage() {
                 preserveAspectRatio="xMidYMid slice"
                 focusable="false"
               >
+                <image href={sellerCraft3} height="500" width="500" />
                 <title>Placeholder</title>
-                <rect
+                {/* <rect
                   width="100%"
                   height="100%"
                   fill="var(--bs-secondary-bg)"
-                />
+                /> */}
                 <text
                   x="50%"
                   y="50%"
